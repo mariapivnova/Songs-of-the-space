89ea3ddb72606269a45b2e5fe255feefcf82eb7e
mariapivnova <mariapivnova@yandex.ru>
2022-12-07 11:35:11 +0300

    commit

---
+++ desktop/src/ru/yoursite/songs_of_the_space/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package ru.yoursite.songs_of_the_space;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import ru.yoursite.songs_of_the_space.MyGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Songs of the Space");
+		new Lwjgl3Application(new MyGame(), config);
+	}
+}
